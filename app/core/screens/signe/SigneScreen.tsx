c757a5057824c56399de5bf22b41cf8d3cd5a5a4
johan1308 <72675791+johan1308@users.noreply.github.com>
2024-06-26 14:56:29 -0400

    Update SigneScreen.tsx

--- app/core/screens/signe/SigneScreen.tsx
+++ app/core/screens/signe/SigneScreen.tsx
@@ -1,15 +1,29 @@
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { StyleSheet, Text, View } from "react-native";
+import { PanResponder, StyleSheet, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Button } from "react-native-paper";
 import tw from "twrnc";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-
-
+import Svg, { Polyline } from "react-native-svg";
+import { useState } from "react";
 
 export const SigneScreen = () => {
+  const [lines, setLines] = useState<any>([]);
+  const [currentLine, setCurrentLine] = useState<any>([]);
 
-
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gestureState) => {
+      setCurrentLine([
+        ...currentLine,
+        { x: gestureState.moveX, y: gestureState.moveY },
+      ]);
+    },
+    onPanResponderRelease: () => {
+      setLines([...lines, currentLine]);
+      setCurrentLine([]);
+    },
+  });
   return (
     <View
       style={[
@@ -17,7 +31,25 @@ export const SigneScreen = () => {
         { flex: 1, justifyContent: "center", alignContent: "center" },
       ]}
     >
-      
+      <View style={styles.container} {...panResponder.panHandlers}>
+        <Svg style={styles.svg}>
+          {lines.map((line: any, index: any) => (
+            <Polyline
+              key={index}
+              points={line.map((p: any) => `${p.x},${p.y}`).join(" ")}
+              fill="none"
+              stroke="black"
+              strokeWidth="3"
+            />
+          ))}
+          <Polyline
+            points={currentLine.map((p:any) => `${p.x},${p.y}`).join(" ")}
+            fill="none"
+            stroke="black"
+            strokeWidth="3"
+          />
+        </Svg>
+      </View>
 
       <Button
         mode="contained"
@@ -45,3 +77,12 @@ const NoImage = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  },
+  svg: {
+    flex: 1,
+  },
+});
